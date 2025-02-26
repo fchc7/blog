40c870983ce0a478c3e8af08f7c223d8b645c80f
jixiang.zhu <jixiang.zhu.ext@siemens.com>
2025-02-26 17:44:18 +0800

    refactor: improve 404 page and layout structure

--- .vitepress/components/NotFound.vue
+++ .vitepress/components/NotFound.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="flex flex-col items-center gap-2 mt-30">
-    <a href="/" class="text-blue-500 i-material-symbols:arrow-warm-up-rounded"></a>
-    <span>404 Not Found</span>
+    <a href="/" class="text-blue-500 i-material-symbols:arrow-warm-up-rounded" title="Go Home"></a>
+    <span class="font-mono">404 Not Found</span>
   </div>
 </template>
--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -12,26 +12,31 @@ const icpNo = ref(import.meta.env.VITE_ICP_NO || 'xxx')
 
 <template>
   <div class="min-h-screen flex flex-col">
-    <!-- 固定顶部 -->
-    <Header class="px-4" />
-    
-    <!-- 主要内容区域 -->
-    <main class="flex-grow my-[60px]">
-      <div class="max-w-[670px] mx-auto px-4 w-full">
-        <NotFound v-if="page.isNotFound" />
-        <PostWrapper v-else-if="page.relativePath.startsWith('posts/')">
-          <Content />
-        </PostWrapper>
-        <Content v-else />
-      </div>
-    </main>
 
-    <!-- 底部备案信息 -->
-    <footer class="w-full py-6 text-center text-sm text-gray-500">
-      <a href="https://beian.miit.gov.cn/" target="_blank" class="max-w-[670px] mx-auto px-4 w-full">
-        © {{  new Date().getFullYear() }} 备案号：{{ icpNo }}
-      </a>
-    </footer>
+    <NotFound v-if="page.isNotFound" /> 
+    <template v-else>
+
+      <!-- 固定顶部 -->
+      <Header class="px-4" />
+      
+      <!-- 主要内容区域 -->
+      <main class="flex-grow my-[60px]">
+        <div class="max-w-[670px] mx-auto px-4 w-full">
+          <PostWrapper v-if="page.relativePath.startsWith('posts/')">
+            <Content />
+          </PostWrapper>
+          <Content v-else />
+        </div>
+      </main>
+
+      <!-- 底部备案信息 -->
+      <footer class="w-full py-6 text-center text-sm text-gray-500">
+        <a href="https://beian.miit.gov.cn/" target="_blank" class="max-w-[670px] mx-auto px-4 w-full">
+          © {{  new Date().getFullYear() }} 备案号：{{ icpNo }}
+        </a>
+      </footer>
+    </template>
+
   </div>
 </template>
 
